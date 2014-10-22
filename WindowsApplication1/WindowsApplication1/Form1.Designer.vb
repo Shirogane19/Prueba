48d9443294699bbcf58b59a3cb3c8777f2dff293
Randall <katana20@hotmail.com>
2014-10-21 21:54:37 -0600

    5

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -38,7 +38,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(163, 197)
+        Me.Label1.Location = New System.Drawing.Point(233, 240)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 1
